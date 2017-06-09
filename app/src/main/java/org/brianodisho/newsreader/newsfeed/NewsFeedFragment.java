e71f93be0226ca8275757897db94b0d0621327d8
brianodisho <brianodisho@gmail.com>
2017-06-08 23:33:42 -0400

    Refactoring

--- app/src/main/java/org/brianodisho/newsreader/newsfeed/NewsFeedFragment.java
+++ app/src/main/java/org/brianodisho/newsreader/newsfeed/NewsFeedFragment.java
@@ -37,7 +37,7 @@ public class NewsFeedFragment extends MvpFragment<NewsFeedView, NewsFeedPresente
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_article_feed, container, false);
+        return inflater.inflate(R.layout.fragment_news_feed, container, false);
     }
 
     @Override
